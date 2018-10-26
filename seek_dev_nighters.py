34de48bace9229162ab09c2f5160b3d57470b513
olegush <43958767+olegush@users.noreply.github.com>
2018-10-26 14:01:46 +0300

    fixed bugs, improved code

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,28 +1,39 @@
 import requests
 import datetime
+from datetime import datetime
+import pytz
 
 
 def load_attempts():
-    pages = 10
-    for page in range(pages):
-        url = 'https://devman.org/api/challenges/solution_attempts/'
-        request = requests.get(url, data={'page': 'page'})
-        users = request.json()['records']
-        for user in users:
+    pages = 1
+    page = 1
+    url = 'https://devman.org/api/challenges/solution_attempts/'
+    while page <= pages:
+        get_att_data = requests.get(url, data={'page': 'page'})
+        att_data_formatted = get_att_data.json()
+        attempts = att_data_formatted['records']
+        pages = att_data_formatted['number_of_pages']
+        page += 1
+        for attempt in attempts:
             yield {
-                'username': user['username'],
-                'timestamp': user['timestamp'],
-                'timezone': 'Europe/Moscow',
+                'username': attempt['username'],
+                'timestamp': attempt['timestamp'],
+                'timezone': attempt['timezone'],
             }
 
 
-def get_midnighters():
-    for user in load_attempts():
-        userdatetime = datetime.datetime.fromtimestamp(user['timestamp'])
-        usertime = userdatetime.strftime('%H:%M:%S')
-        if usertime > '00:00:00' and usertime < '06:00:00':
-            print(user['username'])
+def get_midnighters(load_attempts):
+    midnighters = []
+    for attempt in load_attempts:
+        att_dt = datetime.fromtimestamp(attempt['timestamp'])
+        att_dt_utc = pytz.utc.localize(att_dt)
+        tmz = pytz.timezone(attempt['timezone'])
+        att_dt_local = att_dt_utc.astimezone(tmz)
+        hour = att_dt_local.hour
+        if hour >= 0 and hour < 6 and attempt['username'] not in midnighters:
+            midnighters.append(attempt['username'])
+    return midnighters
 
 
 if __name__ == '__main__':
-    get_midnighters()
+    print(get_midnighters(load_attempts()))
