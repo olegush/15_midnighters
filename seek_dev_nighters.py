9af861579dc2a23319966c8444b84f549bd388de
olegush <43958767+olegush@users.noreply.github.com>
2018-10-28 12:52:22 +0300

    fixed bugs, improved output

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -9,10 +9,10 @@ def load_attempts():
     page = 1
     url = 'https://devman.org/api/challenges/solution_attempts/'
     while page <= pages:
-        get_att_data = requests.get(url, data={'page': 'page'})
-        att_data_formatted = get_att_data.json()
-        attempts = att_data_formatted['records']
-        pages = att_data_formatted['number_of_pages']
+        attempts_data = requests.get(url, data={'page': 'page'})
+        attempts_data_formatted = attempts_data.json()
+        attempts = attempts_data_formatted['records']
+        pages = attempts_data_formatted['number_of_pages']
         page += 1
         for attempt in attempts:
             yield {
@@ -25,15 +25,18 @@ def load_attempts():
 def get_midnighters(load_attempts):
     midnighters = []
     for attempt in load_attempts:
-        att_dt = datetime.fromtimestamp(attempt['timestamp'])
-        att_dt_utc = pytz.utc.localize(att_dt)
-        tmz = pytz.timezone(attempt['timezone'])
-        att_dt_local = att_dt_utc.astimezone(tmz)
-        hour = att_dt_local.hour
+        attempts_dt = datetime.fromtimestamp(attempt['timestamp'],
+                                             pytz.timezone(attempt['timezone'])
+                                             )
+        hour = attempts_dt.hour
         if hour >= 0 and hour < 6 and attempt['username'] not in midnighters:
             midnighters.append(attempt['username'])
     return midnighters
 
 
 if __name__ == '__main__':
-    print(get_midnighters(load_attempts()))
+    midnighters = get_midnighters(load_attempts())
+    if len(midnighters) > 0:
+        print('Night Owl students on DevMan:')
+        for name in midnighters:
+            print(name)
